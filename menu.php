83817a59742e1ed1e5dfec298581dbe2cc9a4575
eduardokaique99 <eduardo_kaique99@hotmail.com>
2023-11-24 19:25:36 -0300

    24/11/23 - att1

---
+++ menu.php
@@ -0,0 +1,33 @@
+<header>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container">
+            <a class="navbar-brand" href="#">TECHINNOV</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menusite"
+                aria-controls="menusite" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="menusite">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="index.php">Inicial</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="sobre.php">Sobre</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="disciplina.php">Disciplinas</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="professor.php">Professores</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="aluno.php">Alunos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="contato.php">Contato</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
---
+++ sobre.php
@@ -0,0 +1,40 @@
+<?php
+
+include __DIR__ . "/header.php";
+
+?>
+
+<main>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12 bg-primary bg-gradient py-5 ">
+                <h1 class="text-center text-light">Sobre nossa empresa</h1>
+            </div>
+        </div>
+    </div>
+    <div class="container py-5">
+        <div class="row">
+            <div class="col-md-6 d-flex flex-column justify-content-center">
+                <h2 class="mb-3">Conheça nossa Hitória</h2>
+
+                <p>Há duas décadas, nasceu a TechInnov Solutions, uma empresa fictícia que se tornaria uma líder inovadora no setor de tecnologia. Fundada por Alice Mendes e Carlos Silva, dois visionários apaixonados por inovação, a empresa teve seu início modesto em uma pequena garagem, onde a dupla trabalhava incansavelmente em projetos de desenvolvimento de software.</p>
+                <p>Desde o início, a TechInnov adotou uma abordagem ágil, abraçando desafios e oportunidades em um mercado tecnológico em constante evolução. Nos primeiros anos, a empresa concentrou-se no desenvolvimento de soluções personalizadas para pequenas empresas locais, construindo uma reputação sólida por sua qualidade e comprometimento.</p>
+                <p>Com o passar do tempo, a TechInnov expandiu seu escopo, diversificando-se para áreas como inteligência artificial, Internet das Coisas (IoT) e cibersegurança. Sua equipe talentosa cresceu significativamente, reunindo mentes criativas e apaixonadas por tecnologia.</p>
+                <p>Um marco crucial na história da TechInnov foi a criação do "InnoHub", um centro de inovação dedicado à pesquisa e desenvolvimento. Esse hub se tornou um ponto focal para colaborações com universidades e startups, impulsionando ainda mais a inovação dentro e fora da empresa.</p>
+                <p>Ao longo dos anos, a TechInnov enfrentou desafios, como a rápida evolução das tecnologias e a competição acirrada. No entanto, sua capacidade de adaptação e a ênfase na criação de soluções orientadas para o cliente permitiram que a empresa não apenas sobrevivesse, mas prosperasse.</p>
+                <p>Atualmente, a TechInnov Solutions é reconhecida globalmente, não apenas por seus produtos e serviços de ponta, mas também por seu compromisso com a responsabilidade social e ambiental. A empresa investe em iniciativas de sustentabilidade e educação, contribuindo para comunidades locais e promovendo uma abordagem ética e inclusiva.</p>
+                <p>A história da TechInnov é um testemunho da visão, inovação e perseverança de seus fundadores e da equipe que cresceu junto com ela. Em um mundo cada vez mais digital, a empresa continua a moldar o futuro da tecnologia, inspirando outras a seguir o caminho da inovação e excelência.</p>
+            </div>
+            <div class="col-md-6">
+                <img src="https://picsum.photos/600/500" class="img-fluid" alt="">
+
+            </div>
+        </div>
+    </div>
+</main>
+
+<?php
+
+include __DIR__ . "/footer.php";
+
+?>
